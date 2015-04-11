f52bcf70a8b5a9f437c50f7ed03eb550036570b9
Emile Sonneveld <Emillo108@yahoo.com>
2015-04-11 04:52:05 +0200

    progress in many domains

---
+++ Assets/cakbite/plant.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class plant : MonoBehaviour {
+
+	int growingState = 0;
+	float growTimer = 0;
+	bool isVisible = false;
+	GameObject player;
+	public float minGrowDistance = 20;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("player");
+	
+	}
+	
+	void OnBecameVisible()
+	{
+		isVisible = true;
+	}
+	void OnBecameInvisible()
+	{
+		isVisible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 distanceToCam = this.transform.position - player.transform.position;
+		if (!isVisible || distanceToCam.magnitude > minGrowDistance){
+
+			if( growTimer>3 && growingState < 3 ){
+				growingState++;
+				growTimer = 0;
+
+				this.transform.position += new Vector3(0, 0.6f, 0);
+
+			}
+			else
+			{
+				growTimer += Time.deltaTime;
+			}
+		}
+
+	}
+
+}
--- Assets/player/Player.cs
+++ Assets/player/Player.cs
@@ -2,36 +2,90 @@ using UnityEngine;
 using System.Collections;
 
 public class Player : MonoBehaviour {
-
-
-	public CharacterController characterController;
+	
+	CharacterController characterController;
 	private float viewDirCam=0;
 	private float viewDirPlayer=0;
 	public float mouseMultiply = 4;
 	public float movementMultiply = 9;
-	public Camera camera;
+	public Camera camera_ref;
+	bool isPlanting = false;
+	float plantingProgress = 0;
+	public int isInPlantingZone = 0;
+	private GameObject oxyGo;
+	float oxygenLevel = 100;
+	Vector3 initialOxyPos;
+
+	public GameObject plant;
 
 	// Use this for initialization
 	void Start () {
-		this.camera = Camera.main;
+		oxyGo = transform.FindChild("CameraUi/oxygen").gameObject;
+		initialOxyPos = oxyGo.transform.localPosition;
+
+		this.camera_ref = Camera.main;
 		characterController=GetComponent<CharacterController>();
 	}
+
+	void OnTriggerEnter(Collider collider){
+		if(collider.gameObject.CompareTag("node")){
+			isInPlantingZone++;
+		}
+	}
 	
+	void OnTriggerExit(Collider collider){
+		if(collider.gameObject.CompareTag("node")){
+			isInPlantingZone=Mathf.Max(isInPlantingZone-1,0);
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
+		if ( oxygenLevel > 0 )
+		{
+			oxygenLevel -= Time.deltaTime * 5;
+			float w = oxygenLevel / 100;
+			oxyGo.transform.localScale = new Vector3(
+				oxyGo.transform.localScale.x, 
+				w,
+				oxyGo.transform.localScale.z
+			);
+			Vector3 tmp = initialOxyPos;
+			tmp.y = initialOxyPos.y + w;
+			oxyGo.transform.localPosition = tmp;
+		}else{
+			oxygenLevel = 0;
+		}
+
 		Vector3 direction = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
 		viewDirCam +=  -Input.GetAxis("Mouse Y") * mouseMultiply;
-		camera.transform.localRotation = Quaternion.Euler(new Vector3(viewDirCam,0,0));
+		camera_ref.transform.localRotation = Quaternion.Euler(new Vector3(viewDirCam,0,0));
 
 		viewDirPlayer +=  Input.GetAxis("Mouse X") * mouseMultiply;
 		this.transform.rotation = Quaternion.Euler(new Vector3(0,viewDirPlayer,0));
 
-
-
 		if ( Input.GetAxis("Jump") > 0.001f){
-			direction += new Vector3(0, 100, 0);
+			direction += new Vector3(0, 10, 0);
 		}
+
+		if(isPlanting)
+			direction*=0;
 		characterController.Move(this.transform.rotation*direction * Time.deltaTime * movementMultiply);
+
+
+		if ( Input.GetAxis("Fire1")>0.001f && !isPlanting && isInPlantingZone > 0 ){
+			isPlanting = true;
+			plantingProgress = 0;
+			Debug.Log ("planting in progress...");
+		}
+
+		if( isPlanting ){
+			plantingProgress += Time.deltaTime;
+			if ( plantingProgress > 5 ){
+				isPlanting = false;
+				Instantiate(plant, this.transform.position + this.transform.forward * 2, plant.transform.rotation);
+			}
+		}
 		
 	}
 
